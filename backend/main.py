877af9fdf936454ef82acb1ebd6579d25b01f6c6
CorneliusKibetDC <kapslogo18@gmail.com>
2025-01-23 16:35:31 +0300

    jwt

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,17 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restx import Api, Resource, fields
 from config import DevConfig
-from models import Notes
+from models import Notes, User
 from exts import db
 from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token, get_jwt_identity, jwt_required
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
 db.init_app(app)  # Initialize the SQLAlchemy extension
 migrate = Migrate(app, db)  # Initialize the Flask-Migrate extension
+JWTManager(app)  # Initialize the Flask-JWT-Extended extension
 api = Api(app, doc='/docs')  # Create the Api instance with Swagger docs enabled
 
 notes_model = api.model(
@@ -20,11 +23,65 @@ notes_model = api.model(
     }
 )
 
+signup_model=api.model(
+    'Signup',
+    {
+         "username": fields.String(),
+        "email": fields.String(),
+        "password": fields.String()
+    }
+)
+login_model=api.model(
+    'Login',
+    {
+         "username": fields.String(),
+        "password": fields.String()
+    }
+)
 # Register a Resource with the Api
 @api.route('/hello')  # Correct way to define the route in Flask-RESTX
 class HelloResource(Resource):
     def get(self):
         return {"message": "Hello, World!"}
+@api.route('/signup')
+class Signup(Resource):
+
+    @api.expect(signup_model)
+    def post(self):
+        data=request.get_json()
+
+        username=data.get("username")
+
+        db_user=User.query.filter_by(username=username).first()
+        if db_user is not None:
+            return jsonify({"message":"Username already exists"})
+        new_user = User(
+            email=data.get("email"),
+            username=data.get("username"),
+            password=generate_password_hash(data.get("password"))
+        )
+        new_user.save()
+        return jsonify({"message": "user created sucessfully"})
+
+
+@api.route('/login')
+class Login(Resource):
+    @api.expect(login_model)
+    def post(self):
+        data=request.get_json()
+        username=data.get("username")
+        password=data.get("password")
+        
+        db_user=User.query.filter_by(username=username).first()
+
+        if db_user and check_password_hash(db_user.password, password):
+            access_token = create_access_token(identity=db_user.username)
+            refresh_token = create_refresh_token(identity=db_user.username)
+            return jsonify(
+                {"access_token": access_token, "refresh_token":refresh_token}
+            )
+                
+
     
 
 @api.route('/notes')
@@ -35,6 +92,8 @@ class NotesResource(Resource):
         notes = Notes.query.all()
         return notes
     @api.marshal_with(notes_model)
+    @api.expect(notes_model)
+    @jwt_required()
     def post(self):
         """Create a new note"""
         data=request.get_json()
@@ -53,6 +112,7 @@ class NoteResource(Resource):
         note=Notes.query.get_or_404(id)
         return note
     @api.marshal_with(notes_model)
+    @jwt_required()
     def put(self, id):
         """Update a note by id"""
         note_to_update = Notes.query.get_or_404(id) 
@@ -63,6 +123,7 @@ class NoteResource(Resource):
         )
         return note_to_update, 200
     @api.marshal_with(notes_model)
+    @jwt_required()
     def delete(self, id):
         """Delete a note by id"""
         note_to_delete = Notes.query.get_or_404(id)
--- backend/models.py
+++ backend/models.py
@@ -32,7 +32,7 @@ password:string
 """
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(255), unique=True, nullable=False)
+    email = db.Column(db.String(255), nullable=False)
     username = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.Text(), nullable=False)
     def __repr__(self):
