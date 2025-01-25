fee60ea374ab8f2b37fb02360131e4e01429b038
CorneliusKibetDC <kapslogo18@gmail.com>
2025-01-25 13:51:52 +0300

    updated

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const HomePage = () => {
     return (
         <div className="home container">
-            <h1>Welcome to the Notes</h1>
+            <h1 className="heading">Welcome to the Notes</h1>
             <Link to="/signup" className="btn btn-primary btn-lg">Get Started</Link>
         
         </div>
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,11 +1,70 @@
-import React from "react";
+import React, {useState} from "react";
+import {Form, Button} from "react-bootstrap";
 
 
 const SignUpPage = () => {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassowrd, setConfirmPassword] = useState("");
+    const submitForm = (e) => {
+        console.log("Form submitted");
+    }
+
+
     return (
-        <div className="signup">
-            <h1>Sign Up Page</h1>
-        
+        <div className="container">
+            <div className="form">
+                <h1>Sign Up Page</h1>
+                <form>
+                    <Form.Group>
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control type="text" 
+                        placeholder="Your username"
+                        value={username}
+                        name="username"
+                        onChange={(e) => {setUsername(e.target.value)}}
+                        />
+                    </Form.Group>
+                    <br></br>
+                    <Form.Group>
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="Your email"
+                        value={email}
+                        name="email"
+                        onChange={(e) => {setEmail(e.target.value)}}
+                        />
+                    </Form.Group>
+                    <br></br>
+                    <Form.Group>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Your password"
+                        value={password}
+                        name="password"
+                        onChange={(e) => {setPassword(e.target.value)}}
+                        />
+                    </Form.Group>
+                    <br></br>
+                    <Form.Group>
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control type="password" placeholder="Your password"
+                        value={confirmPassowrd}
+                        name="confirmPassword"
+                        onChange={(e) => {setConfirmPassword(e.target.value)}}
+                        />
+                    </Form.Group>
+                    <br></br>
+                    <Form.Group>
+                        <Button as="sub" variant="primary" onClick={submitForm}>SignUp</Button>
+                    </Form.Group>
+                    
+
+
+
+
+                </form>
+
+            </div>
         </div>
     )
 }
